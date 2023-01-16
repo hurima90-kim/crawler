5d982da83b1abf4fcf1e633ffd326929a20205d5
hurima90-kim <hurima90@gmail.com>
2023-01-16 22:49:30 +0900

    Reset repository

--- index.js
+++
@@ -1,9 +0,0 @@
-const parse = require('csv-parse/lib/sync');
-const fs = require('fs');
-
-// buffer형식을 string 형식의 2차원 배열로 변경
-const csv = fs.readFileSync('csv/data.csv');
-const records = parse(csv.toString('utf-8'));
-records.forEach((r, i) => {
-    console.log(i, r);
-});
